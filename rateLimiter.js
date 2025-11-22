8f6b7f1284fd12608d9a519b56854bb6a148b40f
legatrun <joaquimmerlo1@gmail.com>
2025-11-22 06:30:25 -0400

    feature: create repository

---
+++ rateLimiter.js
@@ -0,0 +1,37 @@
+const clientBuyRecords = {};
+
+const ONE_MINUTE_IN_MS = 60 * 1000;
+
+function cornRateLimiter(req, res, next) {
+  const clientIp = req.ip;
+  const now = Date.now();
+
+  const lastBuyTime = clientBuyRecords[clientIp];
+
+  if (lastBuyTime) {
+    const timeElapsed = now - lastBuyTime;
+    const timeRemainingSeconds = Math.ceil(
+      (ONE_MINUTE_IN_MS - timeElapsed) / 1000
+    );
+
+    if (timeElapsed < ONE_MINUTE_IN_MS) {
+      // El límite se ha excedido. Devolver 429.
+      console.log(
+        `[RATE LIMIT] IP: ${clientIp} - Compra bloqueada. Tiempo restante: ${timeRemainingSeconds}s`
+      );
+
+      res.setHeader("Retry-After", timeRemainingSeconds);
+
+      return res.status(429).json({
+        error: "Too Many Requests",
+        message: `Too Many Requests 🌽`,
+      });
+    }
+  }
+
+  clientBuyRecords[clientIp] = now;
+
+  next();
+}
+
+module.exports = cornRateLimiter;
---
+++ server.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const cornRateLimiter = require("./rateLimiter");
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST",
+  })
+);
+
+app.use(express.json());
+app.set("trust proxy", 1);
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "¡Welcome to Bob's corn API, use POST /buy-corn 🌽",
+  });
+});
+
+app.post("/buy-corn", cornRateLimiter, (req, res) => {
+  res.status(200).json({
+    message: "¡Successful purchase! 🌽",
+    success: true,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}`);
+});
